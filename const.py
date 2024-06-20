cc0c1e2ff3373740ea86d3888d0c718e6ff1aa15
Juan Pablo Giménez <juan.gimenez@onapsis.com>
2024-06-20 12:47:42 -0300

    old working implementation

--- const.py
+++
@@ -1,9 +0,0 @@
-"""Rosario Bus Constants."""
-
-_RESOURCE = "https://ws.rosario.gob.ar/ubicaciones/public/cuandollega"
-
-DOMAIN = "rosario_bus_transport"
-
-CONF_AGENCY = "agency"
-DEFAULT_AGENCY = "MOVI"
-CONF_ROUTE = "route"
